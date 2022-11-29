5cac7d71db43cffe96333ed451d142c703d286da
Hitesh Kachariya <hiteshk086@gmail.com>
2022-11-29 17:53:41 +0530

    initial commit

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,14 +1,72 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import { styles } from "./Style";
+import InputField from "react-native-input-Field";
+import Button from "react-native-button";
 
 const Login = () => {
   return (
-    <View>
-      <Text>Index</Text>
+    <View style={{ flex: 1, paddingHorizontal: 20 }}>
+      <Image
+        source={require("../src/assets/image1.png")}
+        style={styles.image}
+      />
+      <Text style={styles.mainTitle}>Login to Continue</Text>
+      <Text style={styles.inputTitle}>Mobile Number</Text>
+      <InputField
+        placeholder="Enter Mobile Number"
+        width={"100%"}
+        borderWidth={0.2}
+        keyboardType={"numeric"}
+      />
+      <Text style={styles.inputTitle}>Password</Text>
+      <InputField
+        placeholder="Enter Password"
+        width={"100%"}
+        borderWidth={0.2}
+        showIcon
+      />
+      <TouchableOpacity>
+        <Text style={styles.forgotText}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <Button
+        onPress={() => console.log("Pressed!")}
+        title={"Login"}
+        width={340}
+        height={40}
+        borderRadius={5}
+        backgroundColor={"#5625CC"}
+        fontSize={18}
+        fontWeight={"500"}
+      />
+
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <View
+          style={{ borderBottomWidth: 1, width: "35%", marginVertical: 20 }}
+        />
+        <Text style={{ paddingHorizontal: 10 }}>or </Text>
+        <View
+          style={{ borderBottomWidth: 1, width: "35%", marginVertical: 20 }}
+        />
+      </View>
+      <View style={{ flexDirection: "row", justifyContent: "center" }}>
+        <Text style={{ fontSize: 18, fontWeight: "400" }}>
+          Don't have an account?
+        </Text>
+        <Text style={{ fontSize: 18, color: "#5625CC", fontWeight: "600" }}>
+          {" "}
+          Register
+        </Text>
+      </View>
     </View>
   );
 };
 
 export default Login;
-
-const styles = StyleSheet.create({});
---
+++ src/Style.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  image: { height: 180, width: 350, resizeMode: "stretch" },
+  mainTitle: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#072450",
+    marginBottom: 10,
+  },
+  inputTitle: {
+    fontSize: 16,
+    fontWeight: "500",
+    color: "#072450",
+    marginVertical: 10,
+  },
+  forgotText: {
+    fontSize: 15,
+    fontWeight: "500",
+    marginTop: 10,
+    color: "#5625CC",
+    left: "60%",
+    marginBottom: 20,
+  },
+});
